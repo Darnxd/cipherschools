8f6c77e7023ffca3d90e6c06e15115e64a444398
Gautam Singh <149056911+Darnxd@users.noreply.github.com>
2025-07-10 17:04:56 +0530

    Add files via upload

---
+++ Q04.py
@@ -0,0 +1,9 @@
+length = int(input("enter length : "))
+breadth = int(input("enter breadth : "))
+
+area = length * breadth 
+perimeter = 2 * (length + breadth )
+
+print("Area : " ,area)
+print("perimeter : ", perimeter)
+
