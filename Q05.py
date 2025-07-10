0c302884627ec8f21d1741f2b0a10fc872e2e948
Gautam Singh <149056911+Darnxd@users.noreply.github.com>
2025-07-10 17:05:24 +0530

    Add files via upload

---
+++ Q05.py
@@ -0,0 +1,14 @@
+
+a = int(input("first number: "))
+b = int(input("second number: "))
+
+
+sum = a + b
+diff = a - b
+prod = a * b
+
+
+
+print("Sum:", sum)
+print("Difference:", diff)
+print("Product:", prod)
